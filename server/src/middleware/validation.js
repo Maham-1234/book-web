e68c6ee59f2564fc08667e2c1b04c8134f05c92a
user.name like : 'Maham <user.email like: 'mahamismail1234@gmail.com>
2025-08-11 18:52:34 +0500

    updated category schema- added coerce

--- server/src/middleware/validation.js
+++ server/src/middleware/validation.js
@@ -8,12 +8,14 @@ const validate =
         req.params = await schemas.params.parseAsync(req.params);
       }
       if (schemas.body) {
+        console.log('validating body', req.body);
         req.body = await schemas.body.parseAsync(req.body);
       }
       if (schemas.query) {
         req.query = await schemas.query.parseAsync(req.query);
       }
       // req[key] = await schemas[key].parseAsync(req[key]);
+      console.log('validated');
       next();
     } catch (error) {
       return res.status(400).json({ status: 'fail', errors: error.errors });
